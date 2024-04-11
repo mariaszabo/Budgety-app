cfc7041070f215f60eeccba7b1370fe20e47a27a
mariaszabo <venitdepemarte@gmail.com>
2024-04-11 11:04:56 +0300

    Create BaseLayout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
-function App() {
-  return <div>Hello Budgety</div>;
-}
+import React from "react";
+import BaseLayout from "./layouts/BaseLayout.tsx";
+import { Button, Card } from "antd";
+import { HeartFilled, SmileOutlined } from "@ant-design/icons";
+
+const App = () => (
+  <BaseLayout>
+    <Card>
+      <Button type = "primary">Hi</Button>
+      <HeartFilled />
+    </Card>
+  </BaseLayout> 
+);
 
 export default App;
---
+++ src/layouts/BaseLayout.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Layout, Menu, Button, theme } from 'antd';
+
+const { Header, Sider, Content, Footer } = Layout;
+
+type BaseLayoutProps = { //ca sa o putem folosi pentru fiecare pagina
+  children: React.ReactNode;
+};
+
+const BaseLayout = ({children}: BaseLayoutProps) => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+  return (
+    <Layout style = {{height: "100vh"}}>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              icon: <UserOutlined />,
+              label: 'nav 1',
+            },
+            {
+              key: '2',
+              icon: <VideoCameraOutlined />,
+              label: 'nav 2',
+            },
+            {
+              key: '3',
+              icon: <UploadOutlined />,
+              label: 'nav 3',
+            },
+          ]}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }}>
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: '16px',
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content //acestea sunt setarile pentru continut (un singur card in centru)
+          style={{
+            margin: '24px 16px',
+            // padding: 24,
+            minHeight: 280,
+            // background: colorBgContainer,
+            // borderRadius: borderRadiusLG,
+          }}
+        >
+          {children}
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default BaseLayout;
